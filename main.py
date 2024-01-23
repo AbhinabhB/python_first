de90a060eb9c5c3b917ddf6be0867b9b2d816db8
AbhinabhB <155467762+AbhinabhB@users.noreply.github.com>
2024-01-23 21:43:10 +0530

    Took user input to fill 2 matrixes.

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ def create_matrix(int_val, m, n):
 
 matrix1 = create_matrix(0, 2, 2)
 matrix2 = create_matrix(0, 2, 2)
+
+print("Enter elements for matrix1")
+for i in range(2):
+    for j in range(2):
+        matrix1[i][j] = int(input(f"Enter element for row {i} column {j}: "))
+
+print("Enter elements for matrix2")
+for i in range(2):
+    for j in range(2):
+        matrix2[i][j] = int(input(f"Enter element for row {i} column {j}: "))
