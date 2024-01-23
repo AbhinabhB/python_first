85cc486f2f29f35d29bcdd747b878145d793c65c
AbhinabhB <155467762+AbhinabhB@users.noreply.github.com>
2024-01-23 21:45:01 +0530

    Calculate the result of matrix multiplication.
    Calculated the result of the two matrices using list comprehension and printed the result.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python_first
+
+This a program to print the multiplication of 2 user given matrices.
--- main.py
+++ main.py
@@ -14,3 +14,6 @@ print("Enter elements for matrix2")
 for i in range(2):
     for j in range(2):
         matrix2[i][j] = int(input(f"Enter element for row {i} column {j}: "))
+
+result = [[sum(a * b for a, b in zip(matrix1_row, matrix2_col)) for matrix2_col in zip(*matrix2)] for matrix1_row in matrix1]
+print(result)
